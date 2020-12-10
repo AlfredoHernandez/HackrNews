fa04f079558111a4f72319c00826d26304032c88
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-10 16:50:47 -0600

    Add a `HTTPClient` based on `URLSession`

---
+++ HackrNews/Shared API Infra/URLSessionHTTPClient.swift
@@ -0,0 +1,40 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+
+    public init(session: URLSession) {
+        self.session = session
+    }
+
+    private class UnexpectedValuesRepresentation: Error {}
+
+    private struct URLSessionTaskWrapper: HTTPClientTask {
+        let wrapped: URLSessionTask
+
+        func cancel() {
+            wrapped.cancel()
+        }
+    }
+
+    public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        let task = session.dataTask(with: url) { data, response, error in
+            completion(Result {
+                if let error = error {
+                    throw error
+                } else if let data = data, let response = response as? HTTPURLResponse {
+                    return (data, response)
+                } else {
+                    throw UnexpectedValuesRepresentation()
+                }
+            })
+        }
+        task.resume()
+
+        return URLSessionTaskWrapper(wrapped: task)
+    }
+}
