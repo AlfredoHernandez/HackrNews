2cb79bff9363a7101670a28b0feb6bd1abda5811
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-10 18:27:25 -0600

    Delivers a `Story` with valid JSON

--- HackrNews/Stories API/StoryItemMapper.swift
+++ HackrNews/Stories API/StoryItemMapper.swift
@@ -1,24 +1,46 @@
 //
-//  StoryItemMapper.swift
-//  HackrNews
-//
-//  Created by Jesús Alfredo Hernández Alarcón on 10/12/20.
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
 import Foundation
 
-public class StoryItemMapper {
+public enum StoryItemMapper {
     enum Error: Swift.Error {
         case invalidData
     }
-    
-    public static func map(data: Data, response: HTTPURLResponse) throws {
-        if response.statusCode != 200 {
-            throw Error.invalidData
-        }
-        guard let _ = try? JSONSerialization.jsonObject(with: data) else {
-            throw Error.invalidData
+
+    private struct Item: Decodable {
+        private let id: Int
+        private let title: String
+        private let by: String
+        private let score: Int
+        private let time: Date
+        private let descendants: Int
+        private let kids: [Int]
+        private let type: String
+        private let url: URL
+
+        var story: Story {
+            Story(
+                id: id,
+                title: title,
+                author: by,
+                score: score,
+                createdAt: time,
+                totalComments: descendants,
+                comments: kids,
+                type: type,
+                url: url
+            )
         }
     }
-}
 
+    public static func map(data: Data, response: HTTPURLResponse) throws -> Story {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .secondsSince1970
+        guard response.statusCode == 200, let item = try? decoder.decode(Item.self, from: data) else {
+            throw Error.invalidData
+        }
+        return item.story
+    }
+}
