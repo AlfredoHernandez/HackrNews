fa95c52363ada79226f5015252026a3279497875
J. Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-15 15:59:02 -0600

    Add ViewModel representation

--- HackrNewsiOS/Stories UI/Controllers/LiveHackrNewsRefreshController.swift
+++ HackrNewsiOS/Stories UI/Controllers/LiveHackrNewsRefreshController.swift
@@ -5,6 +5,53 @@
 import HackrNews
 import UIKit
 
+final class LiveHackrNewsModel {
+    private let loader: LiveHackrNewsLoader
+
+    init(loader: LiveHackrNewsLoader) {
+        self.loader = loader
+    }
+
+    private enum State {
+        case pending
+        case loading
+        case loaded([LiveHackrNew])
+        case failed
+    }
+
+    private var state: State = .pending {
+        didSet {
+            onChange?(self)
+        }
+    }
+
+    var onChange: ((LiveHackrNewsModel) -> Void)?
+
+    var isLoading: Bool {
+        switch state {
+        case .loading: return true
+        case .pending, .loaded, .failed: return false
+        }
+    }
+
+    var news: [LiveHackrNew]? {
+        switch state {
+        case let .loaded(news): return news
+        case .pending, .loading, .failed: return nil
+        }
+    }
+
+    func loadNews() {
+        loader.load { [weak self] result in
+            if let news = try? result.get() {
+                self?.state = .loaded(news)
+            } else {
+                self?.state = .failed
+            }
+        }
+    }
+}
+
 final class LiveHackrNewsRefreshController: NSObject {
     private(set) lazy var view: UIRefreshControl = {
         let view = UIRefreshControl()
