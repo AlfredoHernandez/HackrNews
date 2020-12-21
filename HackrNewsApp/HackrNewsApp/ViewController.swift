57ea1bc606922d29220bf4e9fbd5c6297a962667
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-20 19:40:46 -0600

    Add `Hackr News` empty application

---
+++ HackrNewsApp/HackrNewsApp/ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
