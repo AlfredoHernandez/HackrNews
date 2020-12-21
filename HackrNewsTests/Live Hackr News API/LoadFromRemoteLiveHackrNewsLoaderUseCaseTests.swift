b738693c53a94ce4a3b4d98f01b113dd276f4d24
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-21 11:24:04 -0600

    Delivers error on 200 HTTP response with invalid data

--- HackrNewsTests/Live Hackr News API/LoadFromRemoteLiveHackrNewsLoaderUseCaseTests.swift
+++ HackrNewsTests/Live Hackr News API/LoadFromRemoteLiveHackrNewsLoaderUseCaseTests.swift
@@ -52,6 +52,15 @@ final class LoadFromRemoteLiveHackrNewsLoaderUseCaseTests: XCTestCase {
         }
     }
 
+    func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+
+        expect(sut, toCompleteWith: failure(.invalidData), when: {
+            let invalidJSON = Data("invalid json".utf8)
+            client.complete(with: 200, data: invalidJSON)
+        })
+    }
+
     // MARK: Tests helpers
 
     private func makeSUT(
