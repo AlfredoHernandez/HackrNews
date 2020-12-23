1b584a368fdb722e4e24fc8cda88600962cfbe50
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-23 16:20:41 -0600

    On launch, displays remote stories when customer has connectivity

--- HackrNewsApp/HackrNewsApp/SceneDelegate.swift
+++ HackrNewsApp/HackrNewsApp/SceneDelegate.swift
@@ -2,12 +2,22 @@
 //  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
+import HackrNews
+import HackrNewsiOS
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: scene)
+        let httpClient = URLSessionHTTPClient(session: URLSession(configuration: .default))
+        let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json")!
+        let liveHackrNewsloader = RemoteLiveHackrNewsLoader(url: url, client: httpClient)
+        let hackrStoryLoader = RemoteHackrStoryLoader(client: httpClient)
+        let controller = LiveHackrNewsUIComposer.composeWith(liveHackrNewsloader: liveHackrNewsloader, hackrStoryLoader: hackrStoryLoader)
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
     }
 }
--- HackrNewsApp/HackrNewsAppUIAcceptanceTests/HackrNewsAppUIAcceptanceTests.swift
+++ HackrNewsApp/HackrNewsAppUIAcceptanceTests/HackrNewsAppUIAcceptanceTests.swift
@@ -5,8 +5,15 @@
 import XCTest
 
 class HackrNewsAppUIAcceptanceTests: XCTestCase {
-    func test_run_app() throws {
+    func test_onLaunch_displaysRemoteStoriesWhenCustomerHasConnectivity() {
         let app = XCUIApplication()
+
         app.launch()
+
+        let cells = app.cells.matching(identifier: "story-cell")
+        XCTAssertLessThanOrEqual(cells.count, 500)
+
+        let storyTitle = cells.firstMatch.staticTexts.matching(identifier: "story-title-cell").firstMatch
+        XCTAssertTrue(storyTitle.exists)
     }
 }
