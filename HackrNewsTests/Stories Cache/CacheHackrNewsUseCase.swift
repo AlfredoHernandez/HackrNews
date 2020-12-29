fc82531a0bcb41452d3c2842df71f1802bf4ec0b
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-29 12:22:57 -0600

    Extract SUT creation into helper method

--- HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
+++ HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
@@ -2,24 +2,24 @@
 //  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
-import XCTest
 import HackrNews
+import XCTest
 
 class LocalLiveHackrNewsLoader {
     let store: LiveHackrNewsStore
-    
+
     init(store: LiveHackrNewsStore) {
         self.store = store
     }
-    
-    func save(_ news: [LiveHackrNew]) {
+
+    func save(_: [LiveHackrNew]) {
         store.deleteCachedNews()
     }
 }
 
 class LiveHackrNewsStore {
     var deleteCacheStoriesCallCount = 0
-    
+
     func deleteCachedNews() {
         deleteCacheStoriesCallCount += 1
     }
@@ -27,20 +27,25 @@ class LiveHackrNewsStore {
 
 final class CacheHackrNewsUseCase: XCTestCase {
     func test_init_doesNotDeleteCacheUponCreation() {
-        let store = LiveHackrNewsStore()
-
-        _ = LocalLiveHackrNewsLoader(store: store)
+        let (_, store) = makeSUT()
 
         XCTAssertEqual(store.deleteCacheStoriesCallCount, 0)
     }
-    
+
     func test_save_requestsCacheDeletion() {
+        let (sut, store) = makeSUT()
+        let liveHackrNews = [LiveHackrNew(id: 1), LiveHackrNew(id: 2), LiveHackrNew(id: 3)]
+
+        sut.save(liveHackrNews)
+
+        XCTAssertEqual(store.deleteCacheStoriesCallCount, 1)
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT() -> (sut: LocalLiveHackrNewsLoader, store: LiveHackrNewsStore) {
         let store = LiveHackrNewsStore()
         let sut = LocalLiveHackrNewsLoader(store: store)
-        let liveHackrNews = [LiveHackrNew(id: 1), LiveHackrNew(id: 2), LiveHackrNew(id: 3)]
-        
-        sut.save(liveHackrNews)
-        
-        XCTAssertEqual(store.deleteCacheStoriesCallCount, 1)
+        return (sut, store)
     }
 }
