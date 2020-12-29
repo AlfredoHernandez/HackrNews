ffad284f411e03323568424e3845686a53bd7e71
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-29 13:07:00 -0600

    Save command request cache insertion with timestamp on successful deletion

--- HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
+++ HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
@@ -7,15 +7,17 @@ import XCTest
 
 class LocalLiveHackrNewsLoader {
     let store: LiveHackrNewsStore
+    let currentDate: () -> Date
 
-    init(store: LiveHackrNewsStore) {
+    init(store: LiveHackrNewsStore, currentDate: @escaping () -> Date) {
         self.store = store
+        self.currentDate = currentDate
     }
 
     func save(_ news: [LiveHackrNew]) {
-        store.deleteCachedNews { [weak self] error in
+        store.deleteCachedNews { [unowned self] error in
             guard error == nil else { return }
-            self?.store.insertCacheNews(news)
+            self.store.insertCacheNews(news, with: self.currentDate())
         }
     }
 }
@@ -25,14 +27,16 @@ class LiveHackrNewsStore {
     var deleteCacheStoriesCallCount = 0
     var insertCacheStoriesCallCount = 0
     var deletionRequests = [DeletionCompletion]()
+    var insertions = [(news: [LiveHackrNew], timestamp: Date)]()
 
     func deleteCachedNews(completion: @escaping DeletionCompletion) {
         deleteCacheStoriesCallCount += 1
         deletionRequests.append(completion)
     }
 
-    func insertCacheNews(_: [LiveHackrNew]) {
+    func insertCacheNews(_ news: [LiveHackrNew], with timestamp: Date) {
         insertCacheStoriesCallCount += 1
+        insertions.append((news, timestamp))
     }
 
     func completeDeletion(with error: Error, at index: Int = 0) {
@@ -80,11 +84,28 @@ final class CacheHackrNewsUseCase: XCTestCase {
         XCTAssertEqual(store.insertCacheStoriesCallCount, 1)
     }
 
+    func test_save_requestsCacheInsertionWithTimestampOnSuccessfulDeletion() {
+        let timestamp = Date()
+        let (sut, store) = makeSUT(currentDate: { timestamp })
+        let liveHackrNews = anyLiveHackrNews()
+
+        sut.save(liveHackrNews)
+        store.completeDeletionSuccessfully()
+
+        XCTAssertEqual(store.insertions.count, 1)
+        XCTAssertEqual(store.insertions.first?.news, liveHackrNews)
+        XCTAssertEqual(store.insertions.first?.timestamp, timestamp)
+    }
+
     // MARK: - Helpers
 
-    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalLiveHackrNewsLoader, store: LiveHackrNewsStore) {
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: LocalLiveHackrNewsLoader, store: LiveHackrNewsStore) {
         let store = LiveHackrNewsStore()
-        let sut = LocalLiveHackrNewsLoader(store: store)
+        let sut = LocalLiveHackrNewsLoader(store: store, currentDate: currentDate)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(store, file: file, line: line)
         return (sut, store)
