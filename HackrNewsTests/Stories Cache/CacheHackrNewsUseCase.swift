f1b71061239deb10f4772e8b24810a2c1c5d818f
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-29 12:21:10 -0600

    Save command requests cache deletion

--- HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
+++ HackrNewsTests/Stories Cache/CacheHackrNewsUseCase.swift
@@ -3,13 +3,26 @@
 //
 
 import XCTest
+import HackrNews
 
 class LocalLiveHackrNewsLoader {
-    init(store _: LiveHackrNewsStore) {}
+    let store: LiveHackrNewsStore
+    
+    init(store: LiveHackrNewsStore) {
+        self.store = store
+    }
+    
+    func save(_ news: [LiveHackrNew]) {
+        store.deleteCachedNews()
+    }
 }
 
 class LiveHackrNewsStore {
     var deleteCacheStoriesCallCount = 0
+    
+    func deleteCachedNews() {
+        deleteCacheStoriesCallCount += 1
+    }
 }
 
 final class CacheHackrNewsUseCase: XCTestCase {
@@ -19,5 +32,15 @@ final class CacheHackrNewsUseCase: XCTestCase {
         _ = LocalLiveHackrNewsLoader(store: store)
 
         XCTAssertEqual(store.deleteCacheStoriesCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = LiveHackrNewsStore()
+        let sut = LocalLiveHackrNewsLoader(store: store)
+        let liveHackrNews = [LiveHackrNew(id: 1), LiveHackrNew(id: 2), LiveHackrNew(id: 3)]
+        
+        sut.save(liveHackrNews)
+        
+        XCTAssertEqual(store.deleteCacheStoriesCallCount, 1)
     }
 }
