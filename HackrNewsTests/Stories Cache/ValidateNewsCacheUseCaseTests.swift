03fa1a4cba077e59c6d27726bba98ce8bd2d0b87
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2020-12-30 14:40:04 -0600

    Does not delete cache upon creation

---
+++ HackrNewsTests/Stories Cache/ValidateNewsCacheUseCaseTests.swift
@@ -0,0 +1,28 @@
+//
+//  Copyright © 2020 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import HackrNews
+import XCTest
+
+final class ValidateNewsCacheUseCaseTests: XCTestCase {
+    func test_init_doesNotDeleteCacheUponCreation() {
+        let (_, store) = makeSUT()
+
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(
+        currentDate: @escaping () -> Date = Date.init,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) -> (sut: LocalLiveHackrNewsLoader, store: LiveHackrNewsStoreSpy) {
+        let store = LiveHackrNewsStoreSpy()
+        let sut = LocalLiveHackrNewsLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        trackForMemoryLeaks(store, file: file, line: line)
+        return (sut, store)
+    }
+}
