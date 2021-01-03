dbca32ff3bd67720bb1d7ce7581dc22aa74cdf80
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-03 15:24:30 -0600

    Move cache policy into its own file - Now cache validation is a static func due it doesn't hold a state

---
+++ HackrNews/Stories Cache/CachePolicy.swift
@@ -0,0 +1,17 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+final class CachePolicy {
+    private static let calendar = Calendar(identifier: .gregorian)
+    private static var maxCacheAgeInDays: Int { 1 }
+
+    private init() {}
+
+    static func validate(_ timestamp: Date, against date: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
+        return date < maxCacheAge
+    }
+}
