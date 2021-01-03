109421daa2d8329358856e0a0be78c0cf11753d3
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-03 15:27:50 -0600

    Using a struct for `CachePolicy`

--- HackrNews/Stories Cache/CachePolicy.swift
+++ HackrNews/Stories Cache/CachePolicy.swift
@@ -4,11 +4,9 @@
 
 import Foundation
 
-final class CachePolicy {
+struct CachePolicy {
     private static let calendar = Calendar(identifier: .gregorian)
     private static var maxCacheAgeInDays: Int { 1 }
-
-    private init() {}
 
     static func validate(_ timestamp: Date, against date: Date) -> Bool {
         guard let maxCacheAge = calendar.date(byAdding: .day, value: maxCacheAgeInDays, to: timestamp) else { return false }
