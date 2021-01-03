59737bbe4b0a655dcb9e7914fa8cda52d14b75ee
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-03 15:52:34 -0600

    Separate `Date` helpers into distinct contexts

--- HackrNewsTests/Helpers/Shared.swift
+++ HackrNewsTests/Helpers/Shared.swift
@@ -27,17 +27,21 @@ extension HTTPURLResponse {
 }
 
 extension Date {
-    private var cacheMaxAgeInDays: Int { 1 }
-
-    func minusCacheMaxAge() -> Date {
-        adding(days: -cacheMaxAgeInDays)
-    }
-
     func adding(days: Int) -> Date {
         Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
     }
 
     func adding(seconds: TimeInterval) -> Date {
         self + seconds
+    }
+}
+
+// MARK: - Cache Policy Helper
+
+extension Date {
+    private var cacheMaxAgeInDays: Int { 1 }
+
+    func minusCacheMaxAge() -> Date {
+        adding(days: -cacheMaxAgeInDays)
     }
 }
