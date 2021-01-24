a9f4ee0eaa17aa3f809308cf5bd60c0e8ad2de60
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-24 15:59:39 -0600

    Duplicate `RemoteLiveHackrNewsLoader` as `RemoteLoader`

---
+++ HackrNews/Shared API/RemoteLoader.swift
@@ -0,0 +1,42 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public final class RemoteLoader: LiveHackrNewsLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public typealias Result = LiveHackrNewsLoader.Result
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> LiveHackrNewsLoader.Result {
+        do {
+            return .success(try LiveDataMapper.map(data: data, response: response))
+        } catch {
+            return .failure(Error.invalidData)
+        }
+    }
+}
