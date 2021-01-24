f8295053e6af7a33ecb9c968d169e98842d152e7
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-24 14:14:29 -0600

    Duplicate `RemoteHackStoryLoader` as `RemoteStoryCommentsLoader`

---
+++ HackrNews/Live Hackr News API/RemoteStoryCommentsLoader.swift
@@ -0,0 +1,43 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+public final class RemoteStoryCommentsLoader: LiveHackrNewsLoader {
+    private let url: URL
+    private let client: HTTPClient
+
+    public enum Error: Swift.Error {
+        case connectivity
+        case invalidData
+    }
+
+    public typealias Result = LiveHackrNewsLoader.Result
+
+    public init(url: URL, client: HTTPClient) {
+        self.url = url
+        self.client = client
+    }
+
+    public func load(completion: @escaping (Result) -> Void) {
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            switch result {
+            case let .success((data, response)):
+                completion(RemoteStoryCommentsLoader.map(data, from: response))
+            case .failure:
+                completion(.failure(Error.connectivity))
+            }
+        }
+    }
+
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> LiveHackrNewsLoader.Result {
+        do {
+            let items = try StoryCommentsMapper.map(data: data, response: response)
+            return .success(items.map { LiveHackrNew(id: $0) })
+        } catch {
+            return .failure(error)
+        }
+    }
+}
