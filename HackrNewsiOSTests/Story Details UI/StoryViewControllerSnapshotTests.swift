0907d2267007a4e3a9b16288132e3b51aa8f7d0d
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-01-27 15:37:20 -0600

    Add record screens for - Story - Ask - Job - Poll

--- HackrNewsiOSTests/Story Details UI/StoryViewControllerSnapshotTests.swift
+++ HackrNewsiOSTests/Story Details UI/StoryViewControllerSnapshotTests.swift
@@ -7,14 +7,14 @@ import HackrNews
 import XCTest
 
 final class StoryViewControllerSnapshotTests: XCTestCase {
-    func test_emptyStories() {
+    func test_details_story() {
         let cell = StoryCellController(viewModel: StoryDetailViewModel(
             title: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
-            author: "a_username",
-            score: "10 points",
+            author: "dhouston",
+            score: "111 points",
             comments: "0 comments",
             createdAt: "27 years ago",
-            displayURL: "any-url.com"
+            displayURL: "getdropbox.com"
         ))
         let sut = StoryDetailsViewController(storyCellController: cell)
 
@@ -22,5 +22,56 @@ final class StoryViewControllerSnapshotTests: XCTestCase {
 
         assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .light)), named: "story_details_light")
         assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .dark)), named: "story_details_dark")
+    }
+
+    func test_details_ask() {
+        let cell = StoryCellController(viewModel: StoryDetailViewModel(
+            title: "Ask HN: The Arc Effect",
+            author: "tel",
+            score: "25 points",
+            comments: "13 comments",
+            createdAt: "30 minutes ago",
+            displayURL: nil
+        ))
+        let sut = StoryDetailsViewController(storyCellController: cell)
+
+        sut.loadViewIfNeeded()
+
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .light)), named: "ask_story_details_light")
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .dark)), named: "ask_story_details_dark")
+    }
+
+    func test_details_job() {
+        let cell = StoryCellController(viewModel: StoryDetailViewModel(
+            title: "Justin.tv is looking for a Lead Flash Engineer!",
+            author: "justin",
+            score: "6 points",
+            comments: nil,
+            createdAt: "30 minutes ago",
+            displayURL: nil
+        ))
+        let sut = StoryDetailsViewController(storyCellController: cell)
+
+        sut.loadViewIfNeeded()
+
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .light)), named: "job_story_details_light")
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .dark)), named: "job_story_details_dark")
+    }
+
+    func test_details_poll() {
+        let cell = StoryCellController(viewModel: StoryDetailViewModel(
+            title: "Poll: What would happen if News.YC had explicit support for polls?",
+            author: "pg",
+            score: "46 points",
+            comments: "100 comments",
+            createdAt: "30 minutes ago",
+            displayURL: nil
+        ))
+        let sut = StoryDetailsViewController(storyCellController: cell)
+
+        sut.loadViewIfNeeded()
+
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .light)), named: "poll_story_details_light")
+        assert(snapshot: sut.snapshot(for: .iPhone12Mini(style: .dark)), named: "poll_story_details_dark")
     }
 }
