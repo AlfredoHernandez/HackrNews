8a3e470b2f544331a3a2cd4f3d7c0fa449783e06
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-01 11:47:02 -0600

    Finish loading and displays comment

--- HackrNewsTests/Story Details Presentation/CommentPresentationTests.swift
+++ HackrNewsTests/Story Details Presentation/CommentPresentationTests.swift
@@ -2,12 +2,24 @@
 //  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
 //
 
+import HackrNews
 import XCTest
 
-struct CommentViewModel {
+struct CommentLoadingViewModel {
     let isLoading: Bool
 
-    static let loading = CommentViewModel(isLoading: true)
+    static let loading = CommentLoadingViewModel(isLoading: true)
+    static let stoped = CommentLoadingViewModel(isLoading: false)
+}
+
+protocol CommentLoadingView {
+    func display(_ viewModel: CommentLoadingViewModel)
+}
+
+struct CommentViewModel {
+    let author: String
+    let text: String
+    let createdAt: String
 }
 
 protocol CommentView {
@@ -15,44 +27,97 @@ protocol CommentView {
 }
 
 class CommentPresenter {
+    private let calendar: Calendar
+    private let locale: Locale
     private let view: CommentView
-    init(view: CommentView) {
+    private let loadingView: CommentLoadingView
+
+    init(
+        view: CommentView,
+        loadingView: CommentLoadingView,
+        calendar: Calendar = Calendar(identifier: .gregorian),
+        locale: Locale = .current
+    ) {
         self.view = view
+        self.loadingView = loadingView
+        self.calendar = calendar
+        self.locale = locale
     }
 
     func didStartLoadingComment() {
-        view.display(.loading)
+        loadingView.display(.loading)
+    }
+
+    func didFinishLoadingComment(with comment: StoryComment) {
+        loadingView.display(.stoped)
+
+        view.display(CommentViewModel(author: comment.author, text: comment.text, createdAt: formatDate(from: comment.createdAt)))
+    }
+
+    private func formatDate(from date: Date, against: Date = Date()) -> String {
+        let formatter = RelativeDateTimeFormatter()
+        formatter.calendar = calendar
+        formatter.locale = locale
+        return formatter.localizedString(for: date, relativeTo: against)
     }
 }
 
 final class CommentPresentationTests: XCTestCase {
     func test_init_doesNotSendMessagesToView() {
         let view = CommentViewSpy()
-        _ = CommentPresenter(view: view)
+        _ = CommentPresenter(view: view, loadingView: view)
 
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages upon creation")
     }
 
     func test_didStartLoadingComment_displaysLoader() {
         let view = CommentViewSpy()
-        let sut = CommentPresenter(view: view)
+        let sut = CommentPresenter(view: view, loadingView: view)
 
         sut.didStartLoadingComment()
 
         XCTAssertEqual(view.messages, [.display(isLoading: true)])
     }
 
+    func test_didFinishLoadingComment_hidesLoaderAndDisplaysComment() {
+        let view = CommentViewSpy()
+        let calendar = Calendar(identifier: .gregorian)
+        let locale: Locale = .current
+        let sut = CommentPresenter(view: view, loadingView: view, calendar: calendar, locale: locale)
+        let comment = StoryComment(
+            id: 1,
+            author: "an author",
+            comments: [],
+            parent: 0,
+            text: "a text",
+            createdAt: Date().adding(min: -1),
+            type: "comment"
+        )
+
+        sut.didFinishLoadingComment(with: comment)
+
+        XCTAssertEqual(
+            view.messages,
+            [.display(isLoading: false), .display(author: "an author", text: "a text", createdAt: "1 minute ago")]
+        )
+    }
+
     // MARK: - Helpers
 
-    private class CommentViewSpy: CommentView {
+    private class CommentViewSpy: CommentView, CommentLoadingView {
         enum Message: Equatable {
             case display(isLoading: Bool)
+            case display(author: String, text: String, createdAt: String)
         }
 
         var messages = [Message]()
 
-        func display(_ viewModel: CommentViewModel) {
+        func display(_ viewModel: CommentLoadingViewModel) {
             messages.append(.display(isLoading: viewModel.isLoading))
+        }
+
+        func display(_ viewModel: CommentViewModel) {
+            messages.append(.display(author: viewModel.author, text: viewModel.text, createdAt: viewModel.createdAt))
         }
     }
 }
