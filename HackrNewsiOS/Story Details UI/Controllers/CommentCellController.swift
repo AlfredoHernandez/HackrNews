e575a0b56c268a86e5d9e475fc79cdd5acd7a386
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-01 18:19:29 -0600

    Move `CommentCellController` to its own file

---
+++ HackrNewsiOS/Story Details UI/Controllers/CommentCellController.swift
@@ -0,0 +1,37 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import HackrNews
+import UIKit
+
+public protocol CommentCellControllerDelegate {
+    func didRequestComment()
+}
+
+public class CommentCellController: CommentView, CommentLoadingView, CommentErrorView {
+    private(set) var cell: CommentCell?
+    private let delegate: CommentCellControllerDelegate
+
+    public init(delegate: CommentCellControllerDelegate) {
+        self.delegate = delegate
+    }
+
+    func view(in tableView: UITableView) -> CommentCell {
+        cell = tableView.dequeueReusableCell()
+        delegate.didRequestComment()
+        return cell!
+    }
+
+    public func display(_ viewModel: CommentViewModel) {
+        cell?.authorLabel.text = viewModel.author
+        cell?.createdAtLabel.text = viewModel.createdAt
+        cell?.bodyLabel.text = viewModel.text
+    }
+
+    public func display(_ viewModel: CommentLoadingViewModel) {
+        cell?.isLoadingContent = viewModel.isLoading
+    }
+
+    public func display(_: CommentErrorViewModel) {}
+}
--- HackrNewsiOS/Story Details UI/Controllers/StoryDetailsViewController.swift
+++ HackrNewsiOS/Story Details UI/Controllers/StoryDetailsViewController.swift
@@ -5,37 +5,6 @@
 import HackrNews
 import UIKit
 
-public protocol CommentCellControllerDelegate {
-    func didRequestComment()
-}
-
-public class CommentCellController: CommentView, CommentLoadingView, CommentErrorView {
-    private(set) var cell: CommentCell?
-    private let delegate: CommentCellControllerDelegate
-
-    public init(delegate: CommentCellControllerDelegate) {
-        self.delegate = delegate
-    }
-
-    func view(in tableView: UITableView) -> CommentCell {
-        cell = tableView.dequeueReusableCell()
-        delegate.didRequestComment()
-        return cell!
-    }
-
-    public func display(_ viewModel: CommentViewModel) {
-        cell?.authorLabel.text = viewModel.author
-        cell?.createdAtLabel.text = viewModel.createdAt
-        cell?.bodyLabel.text = viewModel.text
-    }
-
-    public func display(_ viewModel: CommentLoadingViewModel) {
-        cell?.isLoadingContent = viewModel.isLoading
-    }
-
-    public func display(_: CommentErrorViewModel) {}
-}
-
 public class StoryDetailsViewController: UITableViewController {
     private var detailsSection: Int { 0 }
     private var storyCell: IndexPath { IndexPath(row: 0, section: detailsSection) }
