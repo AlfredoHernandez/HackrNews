0f04591f841a790336f66bb743f9c4f3811b6f1f
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-09 12:44:30 -0600

    Create `HackrNewsFeedStoreSpecs` that must be imoplemented by any 'Store' test implementation to guarantee it meets specs

--- HackrNewsTests/Stories Cache/CodableHackrNewsFeedStoreTests.swift
+++ HackrNewsTests/Stories Cache/CodableHackrNewsFeedStoreTests.swift
@@ -5,33 +5,10 @@
 import HackrNews
 import XCTest
 
-protocol HackrNewsFeedStoreSpecs {
-    func test_retrieve_deliversEmptyOnEmptyCache()
-    func test_retrieve_hasNoSideEffects()
-    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
-    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
-    func test_insert_overridesPreviouslyInsertedCacheValues()
-    func test_delete_hasNoSideEffectsOnEmptyCache()
-    func test_delete_emptiesPreviousInsertedCache()
-    func test_storeSideEffects_runsSerially()
-}
+typealias FailableHackrNewsFeedStore = HackrNewsFeedStoreSpecs & FailableRetrieveHackrNewsFeedStoreSpecs &
+    FailableInsertHackrNewsFeedStoreSpecs & FailableDeleteHackrNewsFeedStoreSpecs
 
-protocol FailableRetrieveHackrNewsFeedStoreSpecs {
-    func test_retrieve_deliversErrorOnRetrievalError()
-    func test_retrieve_hasNoSideEffectsOnFailure()
-}
-
-protocol FailableInsertHackrNewsFeedStoreSpecs {
-    func test_insert_deliversErrorOnInsertionError()
-    func test_insert_hasNoSideEffectsOnInsertionError()
-}
-
-protocol FailableDeleteHackrNewsFeedStoreSpecs {
-    func test_delete_deliversErrorOnDeletionError()
-    func test_delete_hasNoSideEffectsOnDeletionError()
-}
-
-final class CodableHackrNewsFeedStoreTests: XCTestCase {
+final class CodableHackrNewsFeedStoreTests: XCTestCase, FailableHackrNewsFeedStore {
     override func setUp() {
         super.setUp()
         setupEmptyStoreState()
---
+++ HackrNewsTests/Stories Cache/HackrNewsFeedStoreSpecs.swift
@@ -0,0 +1,31 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import Foundation
+
+protocol HackrNewsFeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache()
+    func test_retrieve_hasNoSideEffects()
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache()
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache()
+    func test_insert_overridesPreviouslyInsertedCacheValues()
+    func test_delete_hasNoSideEffectsOnEmptyCache()
+    func test_delete_emptiesPreviousInsertedCache()
+    func test_storeSideEffects_runsSerially()
+}
+
+protocol FailableRetrieveHackrNewsFeedStoreSpecs: HackrNewsFeedStoreSpecs {
+    func test_retrieve_deliversErrorOnRetrievalError()
+    func test_retrieve_hasNoSideEffectsOnFailure()
+}
+
+protocol FailableInsertHackrNewsFeedStoreSpecs: HackrNewsFeedStoreSpecs {
+    func test_insert_deliversErrorOnInsertionError()
+    func test_insert_hasNoSideEffectsOnInsertionError()
+}
+
+protocol FailableDeleteHackrNewsFeedStoreSpecs: HackrNewsFeedStoreSpecs {
+    func test_delete_deliversErrorOnDeletionError()
+    func test_delete_hasNoSideEffectsOnDeletionError()
+}
