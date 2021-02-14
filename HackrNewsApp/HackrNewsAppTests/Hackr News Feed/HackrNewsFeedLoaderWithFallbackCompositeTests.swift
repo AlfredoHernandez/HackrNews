cb2e3f7cf2764244e47870862bfe43c49a6bd5cb
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-14 13:41:03 -0600

    Move `HackrNewsFeedLoaderWithFallbackComposite` to production

--- HackrNewsApp/HackrNewsAppTests/Hackr News Feed/HackrNewsFeedLoaderWithFallbackCompositeTests.swift
+++ HackrNewsApp/HackrNewsAppTests/Hackr News Feed/HackrNewsFeedLoaderWithFallbackCompositeTests.swift
@@ -3,28 +3,8 @@
 //
 
 import HackrNews
+@testable import HackrNewsApp
 import XCTest
-
-class HackrNewsFeedLoaderWithFallbackComposite: HackrNewsFeedLoader {
-    private let primary: HackrNewsFeedLoader
-    private let fallback: HackrNewsFeedLoader
-
-    init(primary: HackrNewsFeedLoader, fallback: HackrNewsFeedLoader) {
-        self.primary = primary
-        self.fallback = fallback
-    }
-
-    func load(completion: @escaping (HackrNewsFeedLoader.Result) -> Void) {
-        primary.load { [weak self] result in
-            switch result {
-            case let .success(feed):
-                completion(.success(feed))
-            case .failure:
-                self?.fallback.load(completion: completion)
-            }
-        }
-    }
-}
 
 final class HackrNewsFeedLoaderWithFallbackCompositeTests: XCTestCase {
     func test_load_deliversPrimaryFeedOnPrimaryLoaderSuccess() {
