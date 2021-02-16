8090c7de31fb99db2e42076accb160428aae369e
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-16 10:56:42 -0600

    Retrieve command has no side effects

--- HackrNewsTests/Hackr News Feed Cache/Story/Infrastructure/Realm/RealmHackrNewsStoryStoreTests.swift
+++ HackrNewsTests/Hackr News Feed Cache/Story/Infrastructure/Realm/RealmHackrNewsStoryStoreTests.swift
@@ -7,19 +7,42 @@ import XCTest
 
 final class RealmHackrNewsStoryStoreTests: XCTestCase {
     func test_retrieve_deliversEmptyStoryOnEmptyCache() {
-        let sut = RealmHackrNewsStoryStore()
-        let exp = expectation(description: "Wait for retrieve command")
+        let sut = makeSUT()
 
+        expect(sut, toRetrieve: .success(.none))
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+
+        expect(sut, toRetrieve: .success(.none))
+        expect(sut, toRetrieve: .success(.none))
+    }
+
+    // MARK: - Helpers
+
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> RealmHackrNewsStoryStore {
+        let sut = RealmHackrNewsStoryStore()
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+
+    private func expect(
+        _ sut: RealmHackrNewsStoryStore,
+        toRetrieve expectedResult: RealmHackrNewsStoryStore.RetrievalResult,
+        file: StaticString = #filePath,
+        line: UInt = #line
+    ) {
+        let exp = expectation(description: "Wait for retrieve command")
         sut.retrieve(storyID: anyID()) { retrievedResult in
-            switch retrievedResult {
-            case .success(.none):
+            switch (retrievedResult, expectedResult) {
+            case (.success(.none), .success(.none)):
                 break
             default:
-                XCTFail("Expected to retrieve success, got \(retrievedResult) instead")
+                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
             }
             exp.fulfill()
         }
-
         wait(for: [exp], timeout: 1.0)
     }
 }
