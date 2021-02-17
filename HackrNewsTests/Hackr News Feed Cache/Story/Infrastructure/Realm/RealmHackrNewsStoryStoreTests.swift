2b9098f8b1c1c93241f55956f130a0bd3140edb5
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-16 18:33:31 -0600

    Delivers found story on non empty cache

--- HackrNewsTests/Hackr News Feed Cache/Story/Infrastructure/Realm/RealmHackrNewsStoryStoreTests.swift
+++ HackrNewsTests/Hackr News Feed Cache/Story/Infrastructure/Realm/RealmHackrNewsStoryStoreTests.swift
@@ -3,20 +3,45 @@
 //
 
 import HackrNews
+import RealmSwift
 import XCTest
 
 final class RealmHackrNewsStoryStoreTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        Realm.Configuration.defaultConfiguration.inMemoryIdentifier = name
+    }
+
     func test_retrieve_deliversEmptyStoryOnEmptyCache() {
         let sut = makeSUT()
 
-        expect(sut, toRetrieve: .success(.none))
+        expect(sut, withId: anyID(), toRetrieve: .success(.none))
     }
 
     func test_retrieve_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
 
-        expect(sut, toRetrieve: .success(.none))
-        expect(sut, toRetrieve: .success(.none))
+        expect(sut, withId: anyID(), toRetrieve: .success(.none))
+        expect(sut, withId: anyID(), toRetrieve: .success(.none))
+    }
+
+    func test_retrieve_deliversFoundStoryOnNonEmptyCache() {
+        let sut = makeSUT()
+        let story = Story.any
+
+        let exp = expectation(description: "Wait for insertion")
+        sut.insert(story: story.toLocal()) { result in
+            switch result {
+            case .success:
+                break
+            case let .failure(error):
+                XCTFail("Expected to insert given values, but got \(error) instead.")
+            }
+            exp.fulfill()
+        }
+
+        wait(for: [exp], timeout: 1.0)
+        expect(sut, withId: story.id, toRetrieve: .success(story.toLocal()))
     }
 
     // MARK: - Helpers
@@ -29,15 +54,24 @@ final class RealmHackrNewsStoryStoreTests: XCTestCase {
 
     private func expect(
         _ sut: RealmHackrNewsStoryStore,
+        withId id: Int,
         toRetrieve expectedResult: RealmHackrNewsStoryStore.RetrievalResult,
         file: StaticString = #filePath,
         line: UInt = #line
     ) {
         let exp = expectation(description: "Wait for retrieve command")
-        sut.retrieve(storyID: anyID()) { retrievedResult in
+        sut.retrieve(storyID: id) { retrievedResult in
             switch (retrievedResult, expectedResult) {
             case (.success(.none), .success(.none)):
                 break
+            case let (.success(retrievedStory), .success(expectedStory)):
+                XCTAssertEqual(
+                    retrievedStory,
+                    expectedStory,
+                    "Expected \(String(describing: expectedStory)), got \(String(describing: retrievedStory)) instead.",
+                    file: file,
+                    line: line
+                )
             default:
                 XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
             }
