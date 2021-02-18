8aa2380cc1cd27637e15d02d7debdd9b27adeff0
Jesús Alfredo Hernández Alarcón <alfredo.h.alarcon@icloud.com>
2021-02-18 13:58:19 -0600

    Does not load story data upon creation

---
+++ HackrNewsApp/HackrNewsApp/Feed/HackrStoryLoaderWithFallbackComposite.swift
@@ -0,0 +1,9 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import HackrNews
+
+class HackrStoryLoaderWithFallbackComposite {
+    init(primary _: HackrStoryLoader, fallback _: HackrStoryLoader) {}
+}
---
+++ HackrNewsApp/HackrNewsAppTests/Hackr News Feed/HackrStoryLoaderWithFallbackCompositeTests.swift
@@ -0,0 +1,35 @@
+//
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import HackrNews
+@testable import HackrNewsApp
+import XCTest
+
+final class HackrStoryLoaderWithFallbackCompositeTests: XCTestCase {
+    func test_init_doesNotLoadStoryData() {
+        let primary = HackrStoryLoaderSpy()
+        let fallback = HackrStoryLoaderSpy()
+        _ = HackrStoryLoaderWithFallbackComposite(primary: primary, fallback: fallback)
+
+        XCTAssertTrue(primary.loadedStories.isEmpty, "Expected to no request story data")
+        XCTAssertTrue(fallback.loadedStories.isEmpty, "Expected to no request story data")
+    }
+
+    // MARK: - Helpers
+
+    private class HackrStoryLoaderSpy: HackrStoryLoader {
+        var completions = [(id: Int, completion: (HackrStoryLoader.Result) -> Void)]()
+
+        var loadedStories: [Int] { completions.map(\.id) }
+
+        class Task: HackrStoryLoaderTask {
+            func cancel() {}
+        }
+
+        func load(id: Int, completion: @escaping (HackrStoryLoader.Result) -> Void) -> HackrStoryLoaderTask {
+            completions.append((id, completion))
+            return Task()
+        }
+    }
+}
