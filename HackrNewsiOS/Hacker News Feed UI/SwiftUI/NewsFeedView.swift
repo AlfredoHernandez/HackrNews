abcb898e22c16957cb7fef064f481f5cddfcce94
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2021-10-25 17:08:18 -0500

    Add UIKit and SwiftUI modules for news feed

---
+++ HackrNewsiOS/Hacker News Feed UI/SwiftUI/NewsFeedView.swift
@@ -0,0 +1,21 @@
+//
+//  NewsFeedView.swift
+//  HackrNewsiOS
+//
+//  Created by Jesús Alfredo Hernández Alarcón on 25/10/21.
+//  Copyright © 2021 Jesús Alfredo Hernández Alarcón. All rights reserved.
+//
+
+import SwiftUI
+
+struct NewsFeedView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NewsFeedView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewsFeedView()
+    }
+}
