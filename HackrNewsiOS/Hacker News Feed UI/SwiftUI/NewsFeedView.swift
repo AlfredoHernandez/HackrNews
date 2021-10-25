7aadfc97a1a07fd168a6aae6f4e413f806f347f6
Jesús Alfredo Hernández Alarcón <me@alfredohdz.io>
2021-10-25 17:40:44 -0500

    Create `NewsFeedView` view

--- HackrNewsiOS/Hacker News Feed UI/SwiftUI/NewsFeedView.swift
+++ HackrNewsiOS/Hacker News Feed UI/SwiftUI/NewsFeedView.swift
@@ -7,10 +7,49 @@
 //
 
 import SwiftUI
+import HackrNews
+
+struct HackrNewCell: View {
+    let hackrNew: HackrNew
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 4){
+            HStack {
+                Text("by arkadiyt")
+                    .foregroundColor(.secondary)
+                Spacer()
+                Text("1 hour ago")
+                    .foregroundColor(.secondary)
+            }
+            Text("The FBI's internal guide for getting data from AT&T, T-Mobile, Verizon")
+                .font(.title3)
+            Text("www.vice.com")
+            HStack {
+                Text("460 points")
+                Text("111 comments")
+            }
+        }.redacted(reason: .placeholder)
+    }
+}
 
 struct NewsFeedView: View {
+    let news: [HackrNew] = [
+        .init(id: 1),
+        .init(id: 2),
+        .init(id: 3),
+        .init(id: 4),
+        .init(id: 5),
+        .init(id: 6)
+    ]
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List(news, id: \.id) { new in
+                HackrNewCell(hackrNew: new)
+            }
+            .listStyle(.plain)
+            .listRowSeparator(.hidden)
+            .navigationTitle(Text(HackrNewsFeedPresenter.topStoriesTitle))
+        }
     }
 }
 
